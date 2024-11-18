ac2b673da3deb34731f8b95991173205c0e69e66
sarah.morgenstein <sarah.morgenstein@tri-c.edu>
2024-11-18 16:26:04 -0500

    There were a handful of typos or things that you missed, I added comments in your js and HTML based on my findings. The issue with the delete button boiled down to a typo in the property classname when it should be className. There were also a couple of instances where you forgot the () when calling the checkUI function. No need to resubmit!

--- script.js
+++ script.js
@@ -1,173 +1,191 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
-const clearBtn = document.getElementById('clear');
-const itemFilter = document.getElementById('filter');
-const formBtn = itemForm.querySelector('button');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+const formBtn = itemForm.querySelector("button");
 let isEditMode = false;
 
 function displayItems() {
-    const itemsFromStorage = getItemsFromStorage();
-    itemsFromStorage.forEach(item => addItemToDOM(item));
-checkUI;
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDOM(item));
+  // SM - add function call you were missing () after the function name
+  // checkUI;
+
+  checkUI();
 }
 function onAddItemSubmit(e) {
-    e.preventDefault();
-    const newItem = itemInput.value;
-    if (newItem === '') {
-alert ('Please add an item.');
-return;
+  e.preventDefault();
+  const newItem = itemInput.value;
+  if (newItem === "") {
+    alert("Please add an item.");
+    return;
+  }
+
+  // SM - this part was missing from the code
+  // Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector(".edit-mode");
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove("edit-mode");
+    itemToEdit.remove();
+    isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert("That item already exists!");
+      return;
     }
-addItemToDOM(newItem);
-addItemToStorage(newItem);
- checkUI();
- itemInput.value = '';
+  }
+
+  addItemToDOM(newItem);
+  addItemToStorage(newItem);
+  checkUI();
+  itemInput.value = "";
 }
-
-
-
 
 function addItemToDOM(item) {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(item));
-    const button = createButton('remove-item btn-link text-red');
-    li.appendChild(button);
-    itemList.appendChild(li);
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  itemList.appendChild(li);
 }
 
-function getItemsFromStorage () {
-    let itemsFromStorage;
-    if (localStorage.getItem('items') === null) {
-        itemsFromStorage = [];
-    }
+function getItemsFromStorage() {
+  let itemsFromStorage;
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
 
-    else {
-        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
-    }
-
-return itemsFromStorage;
+  return itemsFromStorage;
 }
-
 
 function createButton(classes) {
-    const button = document.createElement('button');
-    button.className = classes;
-    const icon = createIcon('fa-solid fa-xmark');
-    button.appendChild(icon);
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
 
-    return button;
-    
-
+  return button;
 }
 
-function createIcon(classes){
-    const icon = document.createElement('i');
-    icon.classname = classes;
-    return icon;
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  // icon.classname = classes;
+  // corrected from classname to className
+  icon.className = classes;
+  return icon;
 }
 
-function addItemToStorage (item) {
-    let itemsFromStorage;
-    if (localStorage.getItem('items') == null) {
-        itemsFromStorage = [];
-    }
+function addItemToStorage(item) {
+  let itemsFromStorage;
+  if (localStorage.getItem("items") == null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
 
-    else {
-        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
-    }
-
-    itemsFromStorage.push(item);
-    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+  itemsFromStorage.push(item);
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
 }
 
 function onClickItem(e) {
-    if (e.target.parentElement.classList.contains ('remove-item')) {
-        removeItem(e.target.parentElement);
-    }
-    else {
-setItemToEdit(e.target);
-    }
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    // removeItem(e.target.parentElement);
+    //SM - corrected the above line to pass the parentElement.parentElement
+    // your code will remove the button itself - not the li element too
+    removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemToEdit(e.target);
+  }
 }
 
 function setItemToEdit(item) {
-    isEditMode = true;
-    itemList.querySelectorAll('li').forEach(i => i.classList.remove('edit-mode'));
-    item.classList.add('edit-mode'); // dunno why this isn't calling correctly, same problem with my little x boxes
-    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
-    formBtn.style.backgroundColor = '#228B22';
-    itemInput.value = item.textContent;
+  isEditMode = true;
+  itemList
+    .querySelectorAll("li")
+    .forEach((i) => i.classList.remove("edit-mode"));
+  item.classList.add("edit-mode"); // dunno why this isn't calling correctly, same problem with my little x boxes
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  formBtn.style.backgroundColor = "#228B22";
+  itemInput.value = item.textContent;
 }
 
 function removeItem(item) {
-    if (confirm('Are you sure?')) {
-        item.remove();
-        removeItemFromStorage(item.textContent);
-    }
-    checkUI;
-       } 
+  if (confirm("Are you sure?")) {
+    item.remove();
+    removeItemFromStorage(item.textContent);
+    // SM - add call to function with () after the function name
+    // and inside conditional - we don't need to call checkUI if the
+    // item is not being removed.
+    checkUI();
+  }
+  //   checkUI;
+}
 
-       function removeItemFromStorage(item) {
-        let itemsFromStorage = getItemsFromStorage();
-        itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
-        localStorage.setItem('items', JSON.stringify(itemsFromStorage));
-       }
-    
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
 
-    function clearItems() {
-        itemInput.value = '';
-        while (itemList.firstChild) {
-            itemList.removeChild(itemList.firstChild);
-        }
-        localStorage.removeItem('items');
-        checkUI();
-    }
+function clearItems() {
+  itemInput.value = "";
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  localStorage.removeItem("items");
+  checkUI();
+}
 
-    function filterItems(e) {
-        const items = itemList.querySelectorAll('li');
-        const text = e.target.value.toLowerCase();
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
 
-        items.forEach( item => {
+  items.forEach((item) => {
     const itemName = item.firstChild.textContent.toLowerCase();
 
     if (itemName.indexOf(text) != -1) {
-        item.style.display = 'flex';
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
     }
-    else {
-        item.style.display = 'none';
-    }
- });
-        
-    }
-
-    function checkIfItemExists (item) {
-        const itemsFromStorage = getItemsFromStorage();
-       return itemsFromStorage = getItemsFromStorage();
-    }
-
-    function checkUI() {
-        const items = itemList.querySelectorAll('li');
-if (items.length === 0) {
-    clearBtn.style.display = 'none';
-    itemFilter.style.display = 'none';
-} else {
-    clearBtn.style.display = 'block';
-    itemFilter.style.display = 'block';
+  });
 }
-formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
-formBtn.style.backgroundColor = '#333';
 
-isEditMode = false;
-    }
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  //return (itemsFromStorage = getItemsFromStorage());
+  // SM update with correct syntax
+  return itemsFromStorage.includes(item);
+}
 
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = "#333";
 
-    function init() {
-        itemForm.addEventListener('submit', onAddItemSubmit);
-        itemList.addEventListener('click', onClickItem);
-        clearBtn.addEventListener('click', clearItems);
-        itemFilter.addEventListener ('input', filterItems);
-        document.addEventListener('DOMContentLoaded', displayItems);
-        checkUI();
-    }
+  isEditMode = false;
+}
+
+function init() {
+  itemForm.addEventListener("submit", onAddItemSubmit);
+  itemList.addEventListener("click", onClickItem);
+  clearBtn.addEventListener("click", clearItems);
+  itemFilter.addEventListener("input", filterItems);
+  document.addEventListener("DOMContentLoaded", displayItems);
+  checkUI();
+}
 
 init();
 
